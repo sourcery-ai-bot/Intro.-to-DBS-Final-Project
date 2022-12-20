f5d818b79538e1420edeb4fccf2391bc2821ed4f
yiluosha <82742131+yiluosha@users.noreply.github.com>
2022-12-20 11:23:53 +0800

    revise SQL statement

--- python/crypto-221220-b.py
+++ python/crypto-221220-b.py
@@ -342,7 +342,7 @@ class PostgresConnectionClass:
         # 我如何知道依哪一行排序 ?
         # 還是現在這個 function 沒有依任意行排序的功能 ?
         sql_query = 'SELECT * FROM cryptocurrency WHERE date between ' + sdate + ' and ' + edate
-        # sql_query = 'SELECT * FROM cryptocurrency WHERE date between ' + sdate + ' and ' + edate + 'ORDER BY ' + column
+        # sql_query = 'SELECT * FROM cryptocurrency WHERE date between ' + sdate + ' and ' + edate + ' ORDER BY ' + column
 
         # raise NotImplementedError
 
