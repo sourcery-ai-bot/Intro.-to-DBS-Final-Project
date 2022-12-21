578597515690bc05c1c18faf7b6b479ba86e8b43
yiluosha <82742131+yiluosha@users.noreply.github.com>
2022-12-21 20:49:20 +0800

    fetch from google drive

---
+++ auto-update/web_crawler.py
@@ -0,0 +1,47 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait              #explicit wait
+from selenium.webdriver.support import expected_conditions as EC #explicit wait
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
+
+op = webdriver.ChromeOptions()
+op.add_argument('--ignore-certificate-errors-spki-list')
+op.add_argument('--ignore-certificate-errors')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+table=["bitcoin","usd-coin","ethereum","tether","bnb","binance-usd","xrp","dogecoin","cardano","polygon",
+"multi-collateral-dai","polkadot-new","tron","litecoin","shiba-inu","solana","uniswap","avalanche","unus-sed-leo","wrapped-bitcoin"]
+mapping=["Bitcoin","USD Coin","Ethereum","Tether","BNB","Binance USD","XRP","Dogecoin","Cardano","Polygon",
+"Dai","Polkadot","TRON","Litecoin","Shiba Inu","Solana","Uniswap","Avalanche","UNUS SED LEO","Wrapped Bitcoin"]
+db=[]
+def switch_frame(target_crypto):
+    driver.get("https://www.investing.com/crypto/"+target_crypto+"/historical-data")
+    time.sleep(2)
+
+driver.get("https://www.investing.com")
+# time.sleep(30)
+element = WebDriverWait(driver, 1000).until(
+        EC.element_to_be_clickable((By.CLASS_NAME,"popupCloseIcon")) 
+    )
+element.click()
+driver.switch_to.default_content()
+time.sleep(1)
+
+for i in range(20):
+    switch_frame(table[i])
+    tempt=[]
+    tempt.append(mapping[i])
+    datas=driver.find_elements(By.XPATH,'//*[@id="curr_table"]/tbody/tr[1]')
+    for data in datas:
+        tempt.append(data.text)
+    db.append(tempt)
+
+for i in db:
+    for j in i:
+        print(j," ")
+
+driver.quit()
