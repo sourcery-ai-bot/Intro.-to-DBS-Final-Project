2f314a8350c14de636c9fb562cca0392b3418b75
yiluosha <82742131+yiluosha@users.noreply.github.com>
2023-01-02 02:01:05 +0800

    add Updater.py - implement updating

---
+++ auto-update/Updater.py
@@ -0,0 +1,36 @@
+import validate
+import web_crawler2
+import psycopg2
+import datetime
+
+
+class Updater:
+    def __init__(self, conn, cursor):
+        # self.connection = psycopg2.connect(
+        #     f'host={host} dbname={dbname} user={username} password={password}')
+        # self.cursor = self.connection.cursor()
+        self.connection = conn
+        self.cursor = cursor
+
+    def getLastDate(self):
+        sql_query = 'SELECT MAX(date) FROM cryptocurrency'
+        self.cursor.execute(sql_query)
+        return self.cursor.fetchall()[0][0]
+
+    def update(self):
+        self.lastDate = self.getLastDate()
+        if datetime.date.today() > self.lastDate:
+            new_data = web_crawler2.crawl_first()
+            if datetime.datetime.strptime(new_data[0][1], "%b %d, %Y").date() > self.lastDate:
+                new_data = web_crawler2.crawl()
+                for i in new_data:
+                    record = validate.validate(i)
+                    sql_query = f'INSERT INTO cryptocurrency VALUES ({record})'
+                    self.cursor.execute(sql_query)
+                self.connection.commit()
+            else:
+                web_crawler2.driver.quit()
+
+    def disconnect(self):
+        self.cursor.close()
+        self.connection.close()
